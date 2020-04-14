af88ecab35dd42a043a23be595d8cbaa266b1cad
mfierd <muhammad.2789@students.amikom.ac.id>
2020-04-14 12:40:43 +0700

    responsi pemrograman 2789

---
+++ ResponsiPemrograman2789/Program.cs
@@ -0,0 +1,49 @@
+﻿//Program.cs
+using System;
+
+
+namespace Karyawan
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1= new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan karyawan2= new Karyawan(190302124, "Jono", 2000000);
+            Karyawan karyawan3 = new Karyawan(190302125, "Pujo", -1000000);
+
+            Console.WriteLine(" No\t Nik \t Nama \t Gaji Bulanan");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(" 1.  {0}\t{1} \t  {2}", karyawan1.nik, karyawan1.nama, karyawan1.GajiBulanan);
+            Console.WriteLine(" 2.  {0}\t{1} \t  {2}", karyawan2.nik, karyawan2.nama, karyawan2.GajiBulanan);
+            Console.WriteLine(" 3.  {0}\t{1} \t  {2}", karyawan3.nik, karyawan3.nama, karyawan3.GajiBulanan);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Asyiikkkk kenaikan gaji 10%");
+            Console.WriteLine();
+
+            Console.WriteLine(" No\t Nik \t Nama \t Gaji Bulanan");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(" 1.  {0}\t{1} \t  {2}", karyawan1.nik, karyawan1.nama, karyawan1.NaikGaji);
+            Console.WriteLine(" 2.  {0}\t{1} \t  {2}", karyawan2.nik, karyawan2.nama, karyawan2.NaikGaji);
+            Console.WriteLine(" 3.  {0}\t{1} \t  {2}", karyawan3.nik, karyawan3.nama, karyawan3.GajiBulanan);
+
+
+
+            //karyawan.ket = "No  nik    nama             gaji bulanan";
+            //karyawan.nik1 = "190302123 Paijo\t       3.000000";
+            //karyawan.nik2 = "190302124 Jono\t        2.000000";
+
+            //karyawan.KaryawanInfo();
+            //new karyawan1
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+}
---
+++ ResponsiPemrograman2789/karyawan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Karyawan
+{
+    class Karyawan
+    {
+
+        //property
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int NaikGaji { get; set; }
+
+        public Karyawan(int nik, string nama, int gajiBulanan)
+        {
+            this.nik = nik;
+            this.nama = nama;
+
+            if (gajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+                NaikGaji = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+            else
+            {
+                this.GajiBulanan = gajiBulanan;
+                NaikGaji = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+
+
+            //onsole.WriteLine(this.ket);
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("1. " + this.nik1);
+            //onsole.WriteLine("2. " + this.nik2);
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //onsole.WriteLine("Asyiikkkk kenaikan gaji 10%");
+            //onsole.WriteLine();
+
+            //le.WriteLine(this.ket);
+            //onsole.WriteLine("-----------------------------------------------");
+            //onsole.WriteLine("1. " + this.nik1);
+            //Console.WriteLine("2. " + this.nik2);
+
+        }
+
+    }
+}
